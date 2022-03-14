1967a53e8f39c78dc5a0e3ac5161f3fda7c0915b
lucasmbonfim <lucas014bonfim@gmail.com>
2022-03-14 00:48:45 -0300

    Adicionando o projeto ao GitHub

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Funcoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nome = RetornaNomeEIdade("Lucas", "Bonfim");
+            Console.WriteLine(nome);
+
+            MeuMetodo();
+
+            RetornaIdade();
+        }
+
+        static string RetornaNomeEIdade(
+            string nome,
+            string sobrenome,
+            int idade = 20)
+        {
+            // Retorna o nome e sobrenome
+            return nome + " " + sobrenome + " tem " + idade;
+        }
+
+        static void MeuMetodo()
+        {
+            Console.WriteLine("C# é legal!");
+        }
+
+        static void RetornaIdade()
+        {
+            Console.WriteLine("Idade: " + 25);
+        }
+    }
+}
